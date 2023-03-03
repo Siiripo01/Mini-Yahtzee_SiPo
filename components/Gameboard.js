46a9e79fe0418abc956da5b81b3eda59f6f303fe
Siiri Poropudas <n1posi00@students.oamk.fi>
2023-03-03 19:14:58 +0200

    Friday lesson 3.3.23

--- components/Gameboard.js
+++ components/Gameboard.js
@@ -4,6 +4,7 @@ import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import styles from '../style/Style';
 import { NBR_OF_DICES, NBR_OF_THROWS, MAX_SPOT } from '../constants/Game'
 import { Col, Grid } from 'react-native-easy-grid';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 let board = [];
 
@@ -12,6 +13,7 @@ export default Gameboard = ({ route }) => {
     const [playerName, setPlayerName] = useState('');
     const [nbrOfThrowsLeft, setNbrOfThrowsLeft] = useState(NBR_OF_THROWS);
     const [status, setStatus] = useState('');
+    const [scores, setScores] = useState([]);
 
     // This array has the information whether dice is selected or not.
     const [selectedDices, setSelectedDices] = useState(new Array(NBR_OF_DICES).fill(false));
@@ -72,6 +74,18 @@ export default Gameboard = ({ route }) => {
     }
 
     useEffect(() => {
+        if (nbrOfThrowsLeft === 0) {
+            setStatus('Select your points');
+        }
+        else if (nbrOfThrowsLeft <0 ) {
+            setNbrOfThrowsLeft(NBR_OF_THROWS-1);
+        }
+        else if (selectedDicePoints.every(x => x)) {
+            savePlayerPoints();
+        }
+    }, [nbrOfThrowsLeft]);
+
+    useEffect(() => {
         if (playerName === '' && route.params?.player) {
             setPlayerName(route.params.player);
         }
@@ -94,25 +108,6 @@ export default Gameboard = ({ route }) => {
     function getSpotTotal(i) {
         return dicePointsTotal[i];
     }
-
-    // function selectDicePoints(i) {
-    //     let selected = [...selectedDices];
-    //     let selectedPoints = [...selectedDicePoints];
-    //     let points = [...dicePointsTotal];
-
-    //     if (!selectedPoints[i]) {
-    //         selectedDicePoints[i] = true;
-    //         let nbrOfDices = diceSpots.reduce((total, x) => (x === (i + 1) ? total + 1: total), 0);
-    //         points[i] = nbrOfDices * (i + 1);
-    //         setDicePointsTotal(points);
-    //     }
-
-    //     selected.fill(false);
-    //     setSelectedDices(selected);
-    //     setSelectedDicePoints(selectedPoints);
-    //     setNbrOfThrowsLeft(NBR_OF_THROWS)
-    //     return points[i];
-    // }
 
     function selectDicePoints(i) {
         let selected = [...selectedDices];
@@ -145,6 +140,38 @@ export default Gameboard = ({ route }) => {
         setNbrOfThrowsLeft(nbrOfThrowsLeft-1);
         setDiceSpots(spots);
         setStatus('Select and throw dices again');
+    }
+
+    const getScoreboardData = async () => {
+        try {
+            const jsonValue = await AsyncStorage.getItem(SCOREBOAD_KEY)
+            if (jsonValue !== null) {
+                let tmpScores = JSON.parse(jsonValue);
+                setScores(tmpScores);
+            }
+        }
+        catch (error) {
+            console.log(error.message)
+        }
+    }
+
+    const savePlayerPoints = async () => {
+        const playerPoints = {
+            name: playerName,
+            date: '3.3.2023', //replace with real date
+            time: '09:00', //replacev with real time
+            points: 60 // replcae with real points
+        }
+        try { 
+            const newScore = [...scores, playerPoints];
+            const jsonValue= JSON.stringify(newScore);
+            await AsyncStorage.setItem(SCOREBOAD_KEY, jsonValue);
+
+        }
+        catch (error) {
+            console.log(error.message)
+
+        }
     }
 
     // JOTAIN PITÄÄ KORJATA TÄÄLLÄ!
--- components/Scoreboard.js
+++ components/Scoreboard.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import React, {useState, useEffect} from "react";
+import {Text, View} from 'react-native';
+import Header from './Header';
+import Footer from './Footer'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {SCOREBOAD_KEY} from './constants/Game.js'
 
-export default Scoreboard = () => {
+export default ScoreBoard = ( {navigation} ) => {
 
+    const [scores, setScores] = useState([]);
+
+    useEffect(() => {
+        const unsub = navigation.addListener('focus', () =>{
+
+        });
+    }, [navigation])
+
+    const getScoreboardData = async () => {
+        try {
+            const jsonValue = await AsyncStorage.getItem(SCOREBOAD_KEY)
+            if (jsonValue !== null) {
+                let tmpScores = JSON.parse(jsonValue);
+                setScores(tmpScores);
+            }
+        }
+        catch (error) {
+            console.log(error.message);
+        }
+    }
     return (
-        <View>
-            <Text>
-                Scoreboard will be here...
-            </Text>
+        <View style={styles.header}>
+            <Header/>
+            <View>
+                {scores.map((player, i) => (
+                    <Text key={i}>{i + 1}. {player.name} {player.date} {player.time} {player.points}</Text>
+                ))}
+            </View>
         </View>
     )
 }
